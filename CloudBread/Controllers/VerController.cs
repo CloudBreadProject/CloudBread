de5c8dc0d592247e75d7a8524608df64af91ce8a
dwkim <daewoo.kim@microsoft.com>
2016-02-19 10:20:40 +0900

    update-web.config

--- CloudBread/Controllers/VerController.cs
+++ CloudBread/Controllers/VerController.cs
@@ -6,19 +6,19 @@ namespace CloudBread.Controllers
     // Use the MobileAppController attribute for each ApiController you want to use  
     // from your mobile clients 
     [MobileAppController]
-    public class ValuesController : ApiController
+    public class VerController : ApiController
     {
         // GET api/values
         public string Get()
         {
-            return "Hello World!";
+            return "CloudBread ver 2.0.0-dev";
         }
 
         // POST api/values
-        public string Post()
-        {
-            return "Hello World!";
-        }
+        //public string Post()
+        //{
+        //    return "Hello World!";
+        //}
 
         // POST api/values
 
