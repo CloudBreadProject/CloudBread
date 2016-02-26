a41082a255622b54e15506aeec204f2ff9a89b3b
dwkim <daewoo.kim@microsoft.com>
2016-02-27 02:06:39 +0900

    added CloudBread-Socket poejct JWT

--- DataObjects/globalVal.cs
+++ DataObjects/globalVal.cs
@@ -14,6 +14,8 @@ namespace CloudBread.globals
         public static string CloudBreadCryptSetting = WebConfigurationManager.AppSettings["CloudBreadCryptSetting"].ToString();
         public static int conRetryCount = int.Parse(WebConfigurationManager.AppSettings["CloudBreadconRetryCount"]);    /// adding v2.0.0
         public static int conRetryFromSeconds = int.Parse(WebConfigurationManager.AppSettings["CloudBreadconRetryFromSeconds"]);     /// adding v2.0.0
+        public static string CloudBreadSocketKeyText = WebConfigurationManager.AppSettings["CloudBreadSocketKeyText"];     /// adding v2.0.0
+        public static string CloudBreadSocketKeyIV = WebConfigurationManager.AppSettings["CloudBreadSocketKeyIV"];     /// adding v2.0.0
 
     }
 }
