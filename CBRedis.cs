66e8e95f5100d852b192accc17df5f6e1ac7c4f4
dwkim <daewoo.kim@microsoft.com>
2016-02-27 17:50:12 +0900

    added CloudBread-Socket - redis save module

---
+++ CBRedis.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using CloudBread.globals;
+using StackExchange.Redis;
+
+namespace CloudBreadRedis
+{
+    public class CBRedis
+    {
+        // compose connection string 
+        // "dw-cloudbread-redis.redis.cache.windows.net,ssl=true,password=PaCO1A+Nqb54epZR+iByjvR2T3ggi2g7YuxKHphf/eQ="
+        static string redisConnectionString = globalVal.CloudBreadSocketRedisServer + ",ssl=true,password=" +globalVal.CloudBreadSocketRedisPassword;
+
+        public static bool SetRedisKey(string key, string value, int? expTimeMin)    // todo: value as oject or ...?
+        {
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(redisConnectionString);
+
+            // try to connect database
+            try
+            {
+                // StringSet task
+                IDatabase cache = connection.GetDatabase();
+                if (expTimeMin == null)
+                {
+                    // save without expire time
+                    cache.StringSet(key, value);
+                }
+                else
+                {
+                    cache.StringSet(key, value, TimeSpan.FromMinutes(Convert.ToDouble(expTimeMin)));
+                }
+                
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static string GetRedisKeyValue(string key)
+        {
+            string result = "";
+            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(redisConnectionString);
+
+            // try to connect database
+            try
+            {
+                // StringGet task
+                IDatabase cache = connection.GetDatabase();
+                result = cache.StringGet(key);
+
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Controllers/CBSocketTokenValidCheckController.cs
@@ -0,0 +1,95 @@
+﻿/**
+* @file CBSocketTokenValidCheckController
+* @brief This API is used for CloudBread-Socket request token validation from Redis cache service \n
+* https://github.com/CloudBreadProject/CloudBread-Socket \n
+* @param string memberid, string token.guid
+* @return string memberid, string token.guid, string genDateUTC
+* @author Dae Woo Kim
+******************************************************************
+* @todo limit access this API only CloudBread-Socket poejct.
+******************************************************************
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Logger.Logging;
+using CloudBread.globals;
+using CloudBreadLib.BAL.Crypto;
+using Newtonsoft.Json;
+using CloudBreadAuth;
+using CloudBreadRedis;
+using System.Security.Claims;
+
+namespace CloudBread.Controllers
+{
+    [MobileAppController] 
+    public class CBSocketTokenValidCheckController : ApiController
+    {
+        public class Result
+        {
+            public string guid { get; set; }
+            public string sid { get; set; }
+            public string genDateUTC { get; set; }
+        }
+
+        // Payload object
+        public class Token
+        {
+            public string guid { get; set; }
+            public string sid { get; set; }     // logging purpose
+        }
+
+        // GET api/CBSocketTokenValidCheck
+        public Result POST(Token token)
+        {
+            // return
+            Result r = new Result();
+            string redisResult = "";
+
+            /// logging purpose
+            Logging.CBLoggers logMessage = new Logging.CBLoggers();
+            string jsonParam = JsonConvert.SerializeObject(token);
+
+            try
+            {
+                /// fetch redis value by requested token.guid
+                redisResult = CBRedis.GetRedisKeyValue(token.guid);
+
+                if (redisResult == null)
+                {
+                    // does not exist on Redis
+                    r.guid = "";
+                    r.sid = "";
+                    r.genDateUTC = "";
+                }
+                else
+                {
+                    // Deserialize json
+                    r = JsonConvert.DeserializeObject<Result>(redisResult);
+                }
+                return r;
+            }
+
+            catch (Exception ex)
+            {
+                // error log
+                logMessage.memberID = token.sid;        // requested value. Not redis data value.
+                logMessage.Level = "ERROR";
+                logMessage.Logger = "CBSocketTokenValidCheck";
+                logMessage.Message = jsonParam;
+                logMessage.Exception = ex.ToString();
+                Logging.RunLog(logMessage);
+
+                throw;
+            }
+        }
+    }
+}
