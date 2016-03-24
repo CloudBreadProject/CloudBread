c52a8d8421735b42456abf5d22a477ec29a9b576
dwkim <daewoo.kim@microsoft.com>
2016-03-25 01:27:27 +0900

    adding crypt CBComUdtGiftDepositoryController

---
+++ Models/ComUdtGiftDepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudBread.Models
+{
+    public class ComUdtGiftDepositoryInputParams
+    {
+        public string MemberID;
+        public string GiftDepositoryID { get; set; }
+        public string ItemListID { get; set; }
+        public string ItemCount { get; set; }
+        public string FromMemberID { get; set; }
+        public string ToMemberID { get; set; }
+        public string sCol1 { get; set; }
+        public string sCol2 { get; set; }
+        public string sCol3 { get; set; }
+        public string sCol4 { get; set; }
+        public string sCol5 { get; set; }
+        public string sCol6 { get; set; }
+        public string sCol7 { get; set; }
+        public string sCol8 { get; set; }
+        public string sCol9 { get; set; }
+        public string sCol10 { get; set; }
+        public string token { get; set; }
+    }
+}
