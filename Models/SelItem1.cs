cdb7c670dfb4b61f1a1669b5a978b616e36e49e3
dwkim <daewoo.kim@microsoft.com>
2016-03-25 10:51:23 +0900

    adding crypt CBSelItem1Controller

---
+++ Models/SelItem1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudBread.Models
+{
+    public class SelItem1InputParams
+    {
+        public string MemberID { get; set; }     // log purpose
+        public string ItemListID { get; set; }
+        public string token { get; set; }
+    }
+
+    public class SelItem1Model
+    {
+        public string ItemListID { get; set; }
+        public string ItemName { get; set; }
+        public string ItemDescription { get; set; }
+        public string ItemPrice { get; set; }
+        public string ItemSellPrice { get; set; }
+        public string ItemCategory1 { get; set; }
+        public string ItemCategory2 { get; set; }
+        public string ItemCategory3 { get; set; }
+        public string sCol1 { get; set; }
+        public string sCol2 { get; set; }
+        public string sCol3 { get; set; }
+        public string sCol4 { get; set; }
+        public string sCol5 { get; set; }
+        public string sCol6 { get; set; }
+        public string sCol7 { get; set; }
+        public string sCol8 { get; set; }
+        public string sCol9 { get; set; }
+        public string sCol10 { get; set; }
+    }
+}
