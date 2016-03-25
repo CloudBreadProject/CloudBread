b74a3a54430c03cb68f1c70365775474d2fa3415
dwkim <daewoo.kim@microsoft.com>
2016-03-25 13:01:56 +0900

    adding crypt CBUdtMemberGameInfoStageController

---
+++ Models/UdtMemberGameInfoStage.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudBread.Models
+{
+    public class UdtMemberGameInfoStageInputParams
+    {
+        public string InsertORUpdate { get; set; }
+        public string MemberID_MemberGameInfoes { get; set; }
+        public string Level_MemberGameInfoes { get; set; }
+        public string Exps_MemberGameInfoes { get; set; }
+        public string Points_MemberGameInfoes { get; set; }
+        public string UserSTAT1_MemberGameInfoes { get; set; }
+        public string UserSTAT2_MemberGameInfoes { get; set; }
+        public string UserSTAT3_MemberGameInfoes { get; set; }
+        public string UserSTAT4_MemberGameInfoes { get; set; }
+        public string UserSTAT5_MemberGameInfoes { get; set; }
+        public string UserSTAT6_MemberGameInfoes { get; set; }
+        public string UserSTAT7_MemberGameInfoes { get; set; }
+        public string UserSTAT8_MemberGameInfoes { get; set; }
+        public string UserSTAT9_MemberGameInfoes { get; set; }
+        public string UserSTAT10_MemberGameInfoes { get; set; }
+        public string sCol1_MemberGameInfoes { get; set; }
+        public string sCol2_MemberGameInfoes { get; set; }
+        public string sCol3_MemberGameInfoes { get; set; }
+        public string sCol4_MemberGameInfoes { get; set; }
+        public string sCol5_MemberGameInfoes { get; set; }
+        public string sCol6_MemberGameInfoes { get; set; }
+        public string sCol7_MemberGameInfoes { get; set; }
+        public string sCol8_MemberGameInfoes { get; set; }
+        public string sCol9_MemberGameInfoes { get; set; }
+        public string sCol10_MemberGameInfoes { get; set; }
+        public string MemberGameInfoStageID_MemberGameInfoStages { get; set; }
+        public string MemberID_MemberGameInfoStages { get; set; }
+        public string StageName_MemberGameInfoStages { get; set; }
+        public string StageStatus_MemberGameInfoStages { get; set; }
+        public string Category1_MemberGameInfoStages { get; set; }
+        public string Category2_MemberGameInfoStages { get; set; }
+        public string Category3_MemberGameInfoStages { get; set; }
+        public string Mission1_MemberGameInfoStages { get; set; }
+        public string Mission2_MemberGameInfoStages { get; set; }
+        public string Mission3_MemberGameInfoStages { get; set; }
+        public string Mission4_MemberGameInfoStages { get; set; }
+        public string Mission5_MemberGameInfoStages { get; set; }
+        public string Points_MemberGameInfoStages { get; set; }
+        public string StageStat1_MemberGameInfoStages { get; set; }
+        public string StageStat2_MemberGameInfoStages { get; set; }
+        public string StageStat3_MemberGameInfoStages { get; set; }
+        public string StageStat4_MemberGameInfoStages { get; set; }
+        public string StageStat5_MemberGameInfoStages { get; set; }
+        public string sCol1_MemberGameInfoStages { get; set; }
+        public string sCol2_MemberGameInfoStages { get; set; }
+        public string sCol3_MemberGameInfoStages { get; set; }
+        public string sCol4_MemberGameInfoStages { get; set; }
+        public string sCol5_MemberGameInfoStages { get; set; }
+        public string sCol6_MemberGameInfoStages { get; set; }
+        public string sCol7_MemberGameInfoStages { get; set; }
+        public string sCol8_MemberGameInfoStages { get; set; }
+        public string sCol9_MemberGameInfoStages { get; set; }
+        public string sCol10_MemberGameInfoStages { get; set; }
+        public string token { get; set; }
+    }
+}
