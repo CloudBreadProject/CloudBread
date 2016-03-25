48a5b9d239208e826aac045054f15f590d627192
dwkim <daewoo.kim@microsoft.com>
2016-03-25 14:12:24 +0900

    adding crypt CBComInsMemberItemPurchaseController

---
+++ Models/ComInsMemberItemPurchase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudBread.Models
+{
+    public class ComInsMemberItemPurchaseInputParams
+    {
+        public string MemberItemPurchaseID { get; set; }
+        public string MemberID { get; set; }
+        public string ItemListID { get; set; }
+        public string PurchaseQuantity { get; set; }
+        public string PurchasePrice { get; set; }
+        public string PGinfo1 { get; set; }
+        public string PGinfo2 { get; set; }
+        public string PGinfo3 { get; set; }
+        public string PGinfo4 { get; set; }
+        public string PGinfo5 { get; set; }
+        public string PurchaseDeviceID { get; set; }
+        public string PurchaseDeviceIPAddress { get; set; }
+        public string PurchaseDeviceMACAddress { get; set; }
+        public string PurchaseDT { get; set; }
+        public string PurchaseCancelYN { get; set; }
+        public string PurchaseCancelDT { get; set; }
+        public string PurchaseCancelingStatus { get; set; }
+        public string PurchaseCancelReturnedAmount { get; set; }
+        public string PurchaseCancelDeviceID { get; set; }
+        public string PurchaseCancelDeviceIPAddress { get; set; }
+        public string PurchaseCancelDeviceMACAddress { get; set; }
+        public string sCol1 { get; set; }
+        public string sCol2 { get; set; }
+        public string sCol3 { get; set; }
+        public string sCol4 { get; set; }
+        public string sCol5 { get; set; }
+        public string sCol6 { get; set; }
+        public string sCol7 { get; set; }
+        public string sCol8 { get; set; }
+        public string sCol9 { get; set; }
+        public string sCol10 { get; set; }
+        public string token { get; set; }
+    }
+}
