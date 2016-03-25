dfcd4b7e730e24d173d0cdae873abd064ba1ca1d
dwkim <daewoo.kim@microsoft.com>
2016-03-25 12:55:08 +0900

    adding crypt CBUdtGameEventMemberToItemController

--- Controllers/CBUdtGameEventMemberToItemController.cs
+++ Controllers/CBUdtGameEventMemberToItemController.cs
@@ -32,58 +32,41 @@ using CloudBreadAuth;
 using System.Security.Claims;
 using Microsoft.Practices.TransientFaultHandling;
 using Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.SqlAzure;
+using CloudBread.Models;
 
 namespace CloudBread.Controllers
 {
     [MobileAppController]
     public class CBUdtGameEventMemberToItemController : ApiController
     {
-        
-        public class InputParams
+        public HttpResponseMessage Post(UdtGameEventMemberToItemInputParams p)
         {
-            public string InsertORUpdate { get; set; }
-            public string MemberItemID_MemberItems { get; set; }
-            public string MemberID_MemberItems { get; set; }
-            public string ItemListID_MemberItems { get; set; }
-            public string ItemCount_MemberItems { get; set; }
-            public string ItemStatus_MemberItems { get; set; }
-            public string sCol1_MemberItems { get; set; }
-            public string sCol2_MemberItems { get; set; }
-            public string sCol3_MemberItems { get; set; }
-            public string sCol4_MemberItems { get; set; }
-            public string sCol5_MemberItems { get; set; }
-            public string sCol6_MemberItems { get; set; }
-            public string sCol7_MemberItems { get; set; }
-            public string sCol8_MemberItems { get; set; }
-            public string sCol9_MemberItems { get; set; }
-            public string sCol10_MemberItems { get; set; }
-            public string eventID_GameEventMember { get; set; }
-            public string MemberID_GameEventMember { get; set; }
-            public string sCol1_GameEventMember { get; set; }
-            public string sCol2_GameEventMember { get; set; }
-            public string sCol3_GameEventMember { get; set; }
-            public string sCol4_GameEventMember { get; set; }
-            public string sCol5_GameEventMember { get; set; }
-            public string sCol6_GameEventMember { get; set; }
-            public string sCol7_GameEventMember { get; set; }
-            public string sCol8_GameEventMember { get; set; }
-            public string sCol9_GameEventMember { get; set; }
-            public string sCol10_GameEventMember { get; set; }
-
-        }
-
-        public string Post(InputParams p)
-        {
-            string result = "";
+            // try decrypt data
+            if (!string.IsNullOrEmpty(p.token) && globalVal.CloudBreadCryptSetting == "AES256")
+            {
+                try
+                {
+                    string decrypted = Crypto.AES_decrypt(p.token, globalVal.CloudBreadCryptKey, globalVal.CloudBreadCryptIV);
+                    p = JsonConvert.DeserializeObject<UdtGameEventMemberToItemInputParams>(decrypted);
+                }
+                catch (Exception ex)
+                {
+                    ex = (Exception)Activator.CreateInstance(ex.GetType(), "Decrypt Error", ex);
+                    throw ex;
+                }
+            }
 
             // Get the sid or memberID of the current user.
-            var claimsPrincipal = this.User as ClaimsPrincipal;
-            string sid = CBAuth.getMemberID(p.MemberID_MemberItems, claimsPrincipal);
+            string sid = CBAuth.getMemberID(p.MemberID_MemberItems, this.User as ClaimsPrincipal);
             p.MemberID_MemberItems = sid;
             p.MemberID_GameEventMember = sid;
 
             Logging.CBLoggers logMessage = new Logging.CBLoggers();
             string jsonParam = JsonConvert.SerializeObject(p);
+
+            HttpResponseMessage response = new HttpResponseMessage();
+            EncryptedData encryptedResult = new EncryptedData();
+            RowcountResult rowcountResult = new RowcountResult();
 
             try
             {
@@ -100,7 +83,6 @@ namespace CloudBread.Controllers
                 {
                     using (SqlCommand command = new SqlCommand("uspUdtGameEventMemberToItem", connection))
                     {
-
                         command.CommandType = CommandType.StoredProcedure;
                         command.Parameters.Add("@InsertORUpdate ", SqlDbType.NVarChar, -1).Value = p.InsertORUpdate.ToUpper();
                         command.Parameters.Add("@MemberItemID_MemberItems ", SqlDbType.NVarChar, -1).Value = p.MemberItemID_MemberItems;
@@ -136,7 +118,7 @@ namespace CloudBread.Controllers
                         {
                             while (dreader.Read())
                             {
-                                result = dreader[0].ToString();
+                                rowcountResult.result = dreader[0].ToString();
                             }
                             dreader.Close();
                         }
@@ -149,7 +131,24 @@ namespace CloudBread.Controllers
                         logMessage.Message = jsonParam;
                         Logging.RunLog(logMessage);
 
-                        return result;
+                        /// Encrypt the result response
+                        if (globalVal.CloudBreadCryptSetting == "AES256")
+                        {
+                            try
+                            {
+                                encryptedResult.token = Crypto.AES_encrypt(JsonConvert.SerializeObject(rowcountResult), globalVal.CloudBreadCryptKey, globalVal.CloudBreadCryptIV);
+                                response = Request.CreateResponse(HttpStatusCode.OK, encryptedResult);
+                                return response;
+                            }
+                            catch (Exception ex)
+                            {
+                                ex = (Exception)Activator.CreateInstance(ex.GetType(), "Encrypt Error", ex);
+                                throw ex;
+                            }
+                        }
+
+                        response = Request.CreateResponse(HttpStatusCode.OK, rowcountResult);
+                        return response;
                     }
                 }
             }
@@ -167,6 +166,5 @@ namespace CloudBread.Controllers
                 throw;
             }
         }
-
     }
 }
